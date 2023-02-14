ee28c2fa8d4f853d40d9fbd19b7ac1498895c255
Ronny Toribio Blanco <ronnytoribio1@hotmail.com>
2023-02-13 21:26:39 -0500

    Augmenting the renderer with a preload script

---
+++ preload.js
@@ -0,0 +1,8 @@
+const { contextBridge } = require('electron');
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  // can also expose variables not just functions
+})
